66117c08016b75a3d307f53f5db3a767340ff2ab
VladimirPak <vladpak555@gmail.com>
2021-07-27 17:06:35 +0500

    Amends

---
+++ NaitonGps/NaitonGps.Android/Renderers/TintedImageImplement.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using NaitonGps.Droid.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(TintedImageImplement), nameof(NaitonGps.Controls.TintedImage))]
+namespace NaitonGps.Droid.Renderers
+{
+    public class TintedImageImplement : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var effect = (NaitonGps.Controls.TintedImage)Element.Effects.FirstOrDefault(e => e is NaitonGps.Controls.TintedImage);
+
+                if (effect == null || !(Control is ImageView image))
+                    return;
+
+                var filter = new PorterDuffColorFilter(effect.TintColor.ToAndroid(), PorterDuff.Mode.SrcIn);
+                image.SetColorFilter(filter);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        protected override void OnDetached() { }
+    }
+}
---
+++ NaitonGps/NaitonGps/Controls/TintedImage.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace NaitonGps.Controls
+{
+    public class TintedImage : RoutingEffect
+    {
+        public Color TintColor { get; set; }
+        public TintedImage(Color color) : base($"MyElement.{nameof(TintedImage)}")
+        {
+            TintColor = color;
+        }
+    }
+}
