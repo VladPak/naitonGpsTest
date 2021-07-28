88cf4791db77f68f67395a5ff4b40b51f605c7f5
Владимир Пак <vladpak555@gmail.com>
2021-07-28 16:02:04 +0500

    Image Tint renderer for iOS

---
+++ NaitonGps/NaitonGps.iOS/Renderers/ImageTintingRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using NaitonGps.Controls;
+using NaitonGps.iOS.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ImageTinting), typeof(ImageTintingRenderer))]
+namespace NaitonGps.iOS.Renderers
+{
+    public class ImageTintingRenderer : ImageRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+            Control.TintColor = UIColor.Black;
+        }
+    }
+}
---
+++ NaitonGps/NaitonGps/Controls/ImageTinting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NaitonGps.Controls
+{
+    public class ImageTinting : Image
+    {
+    }
+}
