d9f3fdba08292b5dce19ba39fda4d6ae45aee868
VladimirPak <vladpak555@gmail.com>
2021-10-04 19:03:50 +0500

    Logout on Client side, fixes for major popUp (user, settings icons)

--- NaitonGps/NaitonGps/Views/MorePopUp.xaml.cs
+++ NaitonGps/NaitonGps/Views/MorePopUp.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,6 +13,10 @@ namespace NaitonGps.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MorePopUp : Rg.Plugins.Popup.Pages.PopupPage
     {
+        public static double screenWidth { get; } = DeviceDisplay.MainDisplayInfo.Width;
+        public static bool isSmallScreen { get; } = screenWidth <= 480;
+        public static bool isBigScreen { get; } = screenWidth >= 480;
+
         public MorePopUp()
         {
             InitializeComponent();
@@ -21,7 +25,20 @@ namespace NaitonGps.Views
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopPopupAsync();
-            
+        }
+
+        private async void Logout(object sender, EventArgs e)
+        {
+            await Navigation.PopPopupAsync();
+            if (isSmallScreen)
+            {
+                Application.Current.MainPage = new NavigationPage(new LoginScreenNaiton());
+            }
+            else if (isBigScreen)
+            {
+                Application.Current.MainPage = new NavigationPage(new LoginScreenNaitonBigScreen());
+            }
+            await Navigation.PopToRootAsync();
         }
     }
 }
